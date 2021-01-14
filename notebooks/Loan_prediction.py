a457d07400377716218bed7b45f40f6d9f702584
Vincent Villet <vincentvillet@MacBook-Pro-de-Vincent.local>
2021-01-14 12:35:26 +0100

    beginning of sklearn pipeline

--- notebooks/Loan_prediction.py
+++ notebooks/Loan_prediction.py
@@ -27,18 +27,17 @@ from src.constants import files
 import os
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline, make_pipeline, FeatureUnion
+from sklearn.impute import SimpleImputer
 # -
 
 # # Load data
 
 loans_df = pd.read_csv(os.path.join(files.RAW_DATA, files.LOANS_CSV))
 
-loans_df.dtypes
+train_df, test_df = train_test_split(loans_df, test_size=0.2, random_state=1)
 
 # # Preprocessing
 
@@ -46,28 +45,36 @@ loans_df.describe()
 
 loans_df.shape
 
-total = loans_df.isnull().sum().sort_values(ascending=False)
-percent = (loans_df.isnull().sum()/loans_df.isnull().count()).sort_values(ascending=False)
-missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-missing_data.head(20)
+
+def null_values_stats(input_df):
+    total = input_df.isnull().sum().sort_values(ascending=False)
+    percent = (input_df.isnull().sum()/input_df.isnull().count()).sort_values(ascending=False)
+    missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    print(missing_data.head(20))
+
+
+null_values_stats(loans_df)
 
 # ## Fill missing values
 
-loans_df['Gender'] = loans_df['Gender'].fillna(loans_df['Gender'].dropna().mode().values[0] )
-loans_df['Married'] = loans_df['Married'].fillna(loans_df['Married'].dropna().mode().values[0] )
-loans_df['Dependents'] = loans_df['Dependents'].fillna(loans_df['Dependents'].dropna().mode().values[0] )
-loans_df['Self_Employed'] = loans_df['Self_Employed'].fillna(loans_df['Self_Employed'].dropna().mode().values[0] )
-loans_df['LoanAmount'] = loans_df['LoanAmount'].fillna(loans_df['LoanAmount'].dropna().median() )
-loans_df['Loan_Amount_Term'] = loans_df['Loan_Amount_Term'].fillna(loans_df['Loan_Amount_Term'].dropna().mode().values[0] )
-loans_df['Credit_History'] = loans_df['Credit_History'].fillna(loans_df['Credit_History'].dropna().mode().values[0] )
+# +
+pipeline = Pipeline([('imputer', SimpleImputer(strategy="most_frequent")),
+                    ('one_hot_encoding', OneHotEncoder(sparse=False))])
 
-# ### TODO Add scikit learn transformer to fill missing values
-# TODO ajouter slide jupytext
+# TODO: utiliser https://jorisvandenbossche.github.io/blog/2018/05/28/scikit-learn-columntransformer/
 
-total = loans_df.isnull().sum().sort_values(ascending=False)
-percent = (loans_df.isnull().sum()/loans_df.isnull().count()).sort_values(ascending=False)
-missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-missing_data.head(20)
+preprocessed_train = pipeline.fit_transform(train_df)
+# -
+
+preprocessed_train
+
+preprocessed_train_df = pd.DataFrame(preprocessed_train)
+
+preprocessed_train_df.head()
+
+pipeline.named_steps["one_hot_encoding"].get_feature_names()
+
+null_values_stats(preprocessed_train_df)
 
 code_numeric = {'Male': 1, 'Female': 2,
 'Yes': 1, 'No': 2,
