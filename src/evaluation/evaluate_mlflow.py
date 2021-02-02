e1070b578c7367fa6b230d13b3a3e541cca4e983
Pietrowska <sandra.pietrowska@ext.bleckwen.ai>
2021-02-02 12:13:30 +0100

    [ADD] pass paramters to evaluate

--- src/evaluation/evaluate_mlflow.py
+++ src/evaluation/evaluate_mlflow.py
@@ -16,17 +16,20 @@ from sklearn.metrics import f1_score
 
 def evaluate_mlflow():
     test_df = pd.read_csv(os.path.join(files.INTERIM_DATA, files.TEST))
-    run_id = mlflow.active_run().info.run_id
+    current_run_id = mlflow.active_run().info.run_id
 
     logging.info("Loading preprocessing pipeline")
 
-    preprocessing_pipeline = load_latest_preprocessing_pipeline(models.PREPROCESSING_PIPELINE)
+    preprocessing_pipeline, pp_run_id = load_latest_preprocessing_pipeline(models.PREPROCESSING_PIPELINE)
 
+    if pp_run_id != current_run_id:
+        logging.info("Using preprocessing pipeline saved in a previous run")
+        mlflow.log_param("preprocessing_pipeline_run_id", pp_run_id)
     preprocessed_test = preprocessing_pipeline.transform(test_df)
     y_test = test_df[c.Loans.Loan_Status].values
 
     logging.info("Loading trained model")
-    logistic_reg = mlflow.sklearn.load_model(f"runs:/{str(run_id)}/{models.MODEL_NAME}")
+    logistic_reg = mlflow.sklearn.load_model(f"runs:/{str(current_run_id)}/{models.MODEL_NAME}")
 
     logging.info("Computing performance metrics")
     y_pred = logistic_reg.predict(preprocessed_test)
@@ -46,7 +49,7 @@ def load_latest_preprocessing_pipeline(preprocessing_pipeline):
     for run in runs:
         run_id = run.info.run_id
         try:
-            return mlflow.sklearn.load_model(f"runs:/{str(run_id)}/{preprocessing_pipeline}")
+            return mlflow.sklearn.load_model(f"runs:/{str(run_id)}/{preprocessing_pipeline}"), run_id
         except IOError:
             continue
 
