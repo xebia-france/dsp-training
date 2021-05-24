f7631a120ffcfe2689d72ec01be92c1c2e9208e1
Vincent Villet <vincentvillet@MacBook-Pro-de-Vincent.local>
2021-05-24 12:26:32 +0200

    remove bool_dict from main and function load_latest_preprocessing_pipeline from predict.py

--- main.py
+++ main.py
@@ -12,12 +12,10 @@ import src.constants.files as files
 import src.constants.models as m
 
 
-def main(bool_dict):
+def main():
     """
     Launch all project steps.
 
-    :param bool_dict: Dictionnary with step names as keys and boolean as values allowing to bypass steps. This can be
-    useful to re-run all steps but model training steps if they are already done for example.
     :return:
     """
     download_file_from_url(files.LOANS_DATA_URL, files.LOANS)
@@ -25,40 +23,29 @@ def main(bool_dict):
     today_str = str(datetime.date(datetime.now()))
     mlflow.set_experiment(files.MLFLOW_EXPERIMENT_NAME)
     with mlflow.start_run(run_name=today_str):
-        if bool_dict["load_and_split"]:
-            logging.info("*********** 1/5 Loading and splitting data ***********")
-            load_and_split_data(raw_data_path=files.LOANS,
-                                training_file_path=files.TRAIN,
-                                test_file_path=files.TEST)
+        logging.info("*********** 1/5 Loading and splitting data ***********")
+        load_and_split_data(raw_data_path=files.LOANS,
+                            training_file_path=files.TRAIN,
+                            test_file_path=files.TEST)
 
-        if bool_dict["preprocess"]:
-            logging.info("*********** 2/5 Preprocessing ***********")
-            preprocess(training_file_path=files.TRAIN,
-                       preprocessed_train_path=files.PREPROCESSED_TRAIN,
-                       preprocessing_pipeline_name=files.PREPROCESSING_PIPELINE)
+        logging.info("*********** 2/5 Preprocessing ***********")
+        preprocess(training_file_path=files.TRAIN,
+                   preprocessed_train_path=files.PREPROCESSED_TRAIN,
+                   preprocessing_pipeline_name=files.PREPROCESSING_PIPELINE)
 
-        if bool_dict["logistic_reg_train"]:
-            logging.info("*********** 3/5 Modeling ***********")
-            logistic_reg_train(preprocessed_train_path=files.PREPROCESSED_TRAIN,
-                               logistic_reg_model_name=m.LOGISTIC_REG_MODEL_NAME)
+        logging.info("*********** 3/5 Modeling ***********")
+        logistic_reg_train(preprocessed_train_path=files.PREPROCESSED_TRAIN,
+                           logistic_reg_model_name=m.LOGISTIC_REG_MODEL_NAME)
 
-        if bool_dict["predict"]:
-            logging.info("*********** 4/5 Prediction ***********")
-            predict(test_file_path=files.TEST,
-                    preprocessing_pipeline_name=files.PREPROCESSING_PIPELINE,
-                    logistic_reg_model_name=m.LOGISTIC_REG_MODEL_NAME,
-                    prediction_file_path=files.PREDICTIONS_TEST)
+        logging.info("*********** 4/5 Prediction ***********")
+        predict(test_file_path=files.TEST,
+                preprocessing_pipeline_name=files.PREPROCESSING_PIPELINE,
+                logistic_reg_model_name=m.LOGISTIC_REG_MODEL_NAME,
+                prediction_file_path=files.PREDICTIONS_TEST)
 
-        if bool_dict["evaluate"]:
-            logging.info("*********** 5/5 Evaluation ***********")
-            evaluate(prediction_file_path=files.PREDICTIONS_TEST)
+        logging.info("*********** 5/5 Evaluation ***********")
+        evaluate(prediction_file_path=files.PREDICTIONS_TEST)
 
 
 if __name__ == "__main__":
-    bool_dict = {"load_and_split": True,
-                 "preprocess": True,
-                 "logistic_reg_train": True,
-                 "predict":True,
-                 "evaluate": True
-                 }
-    main(bool_dict)
+    main()
--- src/predict/predict.py
+++ src/predict/predict.py
@@ -2,10 +2,6 @@ import os
 import pandas as pd
 import logging
 import mlflow
-from mlflow.entities import ViewType
-from mlflow.tracking import MlflowClient
-
-import src.constants.files as files
 
 
 def predict(test_file_path, preprocessing_pipeline_name, logistic_reg_model_name, prediction_file_path):
@@ -21,20 +17,17 @@ def predict(test_file_path, preprocessing_pipeline_name, logistic_reg_model_name
     """
     test_df = pd.read_csv(test_file_path)
 
-    current_run_id = mlflow.active_run().info.run_id
+    current_run = mlflow.active_run()
 
     logging.info("Loading preprocessing pipeline")
-    preprocessing_pipeline, pp_run_id = load_latest_preprocessing_pipeline(
-        preprocessing_pipeline_name, files.MLFLOW_EXPERIMENT_NAME)
-    if pp_run_id != current_run_id:
-        logging.info("Using preprocessing pipeline saved in a previous run")
-        mlflow.log_param("preprocessing_pipeline_run_id", pp_run_id)
+    preprocessing_pipeline = mlflow.sklearn.load_model(
+        os.path.join(current_run.info.artifact_uri, preprocessing_pipeline_name))
 
     preprocessed_test = preprocessing_pipeline.transform(test_df)
 
     logging.info("Loading trained model")
     logistic_reg = mlflow.sklearn.load_model(
-        os.path.join(mlflow.active_run().info.artifact_uri, logistic_reg_model_name))
+        os.path.join(current_run.info.artifact_uri, logistic_reg_model_name))
 
     logging.info(f"Make predictions with {logistic_reg_model_name}")
     y_pred = logistic_reg.predict(preprocessed_test)
@@ -43,25 +36,3 @@ def predict(test_file_path, preprocessing_pipeline_name, logistic_reg_model_name
 
     logging.info("Saving prediction results")
     test_df.to_csv(prediction_file_path, index=False)
-
-
-def load_latest_preprocessing_pipeline(preprocessing_pipeline_name, experiment_name):
-    experiment_id = mlflow.get_experiment_by_name(experiment_name).experiment_id
-
-    runs = MlflowClient().search_runs(
-        experiment_ids=str(experiment_id),
-        run_view_type=ViewType.ACTIVE_ONLY,
-        order_by=["tag.start_time DESC"]
-    )
-
-    for run in runs:
-        run_id = run.info.run_id
-        artifact_uri = run.info.artifact_uri
-        try:
-            return mlflow.sklearn.load_model(os.path.join(artifact_uri, preprocessing_pipeline_name)), run_id
-        except:
-            continue
-
-    raise Exception(
-        f"Could not find a preprocessing pipeline named {preprocessing_pipeline_name} in current and previous runs"
-    )
